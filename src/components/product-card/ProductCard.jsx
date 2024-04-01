558a602474c2598ced9e0d3d49ca86ad853c1313
Aleksander Storum <alesto95654@stud.noroff.no>
2024-04-01 13:22:59 +0200

    Deleted some marked out code

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -24,23 +24,4 @@ const ProductCard = ({ product }) => {
   );
 };
 
-// const ProductCard = ({ product }) => {
-//   return (
-//     <S.Card>
-//       <S.ImageContainer>
-//         <S.Image src={product.image.url} alt={product.title} />
-//       </S.ImageContainer>
-//       <S.Info>
-//         <S.Title>{product.title}</S.Title>
-//         <S.Price>
-//           <DiscountInfo price={product.price} discountedPrice={product.discountedPrice} showDiscountPercentage={false} />
-//         </S.Price>
-//         <Link to={`/product/${product.id}`}>
-//           <S.Button>View Product</S.Button>
-//         </Link>
-//       </S.Info>
-//     </S.Card>
-//   );
-// };
-
 export default ProductCard;
